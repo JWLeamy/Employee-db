a7e4ae8c6707084e62e33d7a5729c37eda2b8b06
John Leamy <jwleamy@gmail.com>
2023-02-08 14:35:59 -0800

    s

--- db/index.js
+++ db/index.js
@@ -206,4 +206,170 @@ function getRolesToDelete (){
 
 /* ----------All Department related functions-----------*/
 
+function deleteDepartment(department) {
+    mysqlConnection.query('DELETE FROM department WHERE department_name = ?;', department, function (err, results) {
+        console.table(results);
+        start.start();
+    })
+}
 
+function getDepartmentsToDelete (){
+    mysqlConnection.query('SELECT department_name FROM department', function (err, results) {
+        departmentsArr.length = 0;
+        for (const department in results) {
+            if (departmentsArr.indexOf(results[department].department_name) === -1) {
+                departmentsArr.push(results[department].department_name);
+            }
+        }
+        inquirer.prompt(deleteDepartmentQs)
+        .then((response) => {
+            deleteDepartment(response.department);
+        });        
+    })
+}
+
+/* ----------All Manager related functions-----------*/
+
+function updateManager(name, manager){
+    const employeeName = name.split(" ");
+    const managerName = manager.split(" ");
+    mysqlConnection.query('UPDATE employee SET manager_id = (SELECT a.id FROM (SELECT b.id FROM employee b WHERE b.first_name = ? AND b.last_name = ?) a) WHERE first_name = ? AND last_name = ?;', [managerName[0], managerName[1], employeeName[0], employeeName[1]], function (err, results) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(results);
+            console.log("Employee's manager has been successfully updated to database.");
+            start.start(); 
+        }
+})};
+
+function updateManagerPrompt() {
+    inquirer.prompt(updateEmployeeManagerQs)
+    .then((response) => {
+        updateManager(response.employee, response.manager);
+    });
+}
+
+function selectManager(){
+    mysqlConnection.query('SELECT concat(first_name, " ", last_name) AS employee_name FROM employee;', function (err, results) {
+        employeesArr.length = 0;
+        for (const person in results) {
+            if (employeesArr.indexOf(results[person].employee_name) === -1) {
+                employeesArr.push(results[person].employee_name);
+            }
+        }
+    })
+    mysqlConnection.query('SELECT concat(b.first_name, " ", b.last_name) AS manager_name FROM employee a LEFT JOIN employee b ON a.manager_id = b.id INNER JOIN roles c ON a.role_id = c.id INNER JOIN department d ON d.id = c.department_id WHERE b.first_name IS NOT NULL AND b.last_name IS NOT NULL;', function (err, results) {
+        managersArr.length = 0;
+        for (const person in results) {
+            if (managersArr.indexOf(results[person].manager_name) === -1) {
+                managersArr.push(results[person].manager_name);
+            }
+        }
+        managersArr.push("None");
+        updateManagerPrompt();  
+    });
+};
+
+/* ----------All Employee related functions-----------*/
+
+
+function updateEmployee(name, role){
+    const employeeName = name.split(" ");
+    mysqlConnection.query('UPDATE employee SET role_id = (SELECT r.id FROM roles r WHERE r.role_title = ?) WHERE employee.first_name = ? AND employee.last_name = ?;', [role, employeeName[0], employeeName[1]], function (err, results) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(results);
+            console.log("Employee has been successfully updated to database.");
+            start.start(); 
+        }
+})};
+
+function updateEmployeeRolePrompt() {
+    inquirer.prompt(updateEmployeeQ)
+    .then((response) => {
+        updateEmployee(response.employee, response.role);
+    });
+}
+
+function selectEmployeeToUpdate(){
+    mysqlConnection.query('SELECT concat(first_name, " ", last_name) AS employee_name FROM employee;', function (err, results) {
+        employeesArr.length = 0;
+        for (const person in results) {
+            if (employeesArr.indexOf(results[person].employee_name) === -1) {
+                employeesArr.push(results[person].employee_name);
+            }
+        }
+    })
+    mysqlConnection.query('SELECT role_title FROM roles', function (err, results) {
+        rolesArr.length = 0;
+        for (const role in results) {
+            if (rolesArr.indexOf(results[role].role_title) === -1) {
+                rolesArr.push(results[role].role_title);
+            }  
+        }
+        updateEmployeeRolePrompt();   
+    });
+};
+
+function addEmployee(fName, lName, role, manager) {
+
+    const managerName = manager.split(" ");
+
+    mysqlConnection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, (SELECT r.id FROM roles r WHERE r.role_title = ?), (SELECT e.id FROM employee e WHERE e.first_name = ? AND e.last_name = ?));', [fName, lName, role, managerName[0], managerName[1]], function (err, results) {
+        console.log(err);
+        console.log(results);
+        console.log("This employee has been successfully added to the database!");
+        start.start();  
+    })
+}
+
+function addEmployeePrompt(){
+    mysqlConnection.query('SELECT concat(b.first_name, " ", b.last_name) AS manager_name FROM employee a LEFT JOIN employee b ON a.manager_id = b.id INNER JOIN roles c ON a.role_id = c.id INNER JOIN department d ON d.id = c.department_id WHERE b.first_name IS NOT NULL AND b.last_name IS NOT NULL;', function (err, results) {
+        managersArr.length = 0;
+        for (const person in results) {
+            if (managersArr.indexOf(results[person].manager_name === -1)) {
+                managersArr.push(results[person].manager_name);
+            }
+        }
+        managersArr.push("None");
+    });    
+    mysqlConnection.query('SELECT role_title FROM roles', function (err, results) {
+        rolesArr.length = 0;
+        for (const role in results) {
+            if (rolesArr.indexOf(results[role].role_title) === -1) {
+                rolesArr.push(results[role].role_title);
+            }
+        }     
+    });
+    inquirer.prompt(addEmployeeQs)
+    .then((response) => {
+        addEmployee(response.fName, response.lName, response.role, response.manager);
+    });
+}
+
+/* ----------Add, Change, and target specific roles within Department-----------*/
+
+function addRole(role, salary, department) {
+    mysqlConnection.query("INSERT INTO roles (role_title, salary, department_id) VALUES (?, ?, (SELECT id FROM department WHERE department_name = ?));", [role, Number(salary), department], function (err, results) {
+        console.log(results);
+        console.log("This new role has been successfully added to the database!");
+        start.start();  
+    })
+}
+
+function addRolePrompt(){
+    mysqlConnection.query('SELECT department_name FROM department', function (err, results) {   
+        departmentsArr.length = 0;
+        for (const department in results) {
+            if (departmentsArr.indexOf(results[department].department_name) === -1) {
+                departmentsArr.push(results[department].department_name);
+            }
+        }  
+    })
+    inquirer.prompt(addRoleQs)
+    .then((response) => {
+        addRole(response.role, response.salary, response.department);
+    });
+}
