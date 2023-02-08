61178279a94d9789eda55575aa72899b390860e6
John Leamy <jwleamy@gmail.com>
2023-02-08 13:59:25 -0800

    inde.js functions

--- db/index.js
+++ db/index.js
@@ -1,8 +1,12 @@
-
+//import inquireer
+const inquirer = require('inquirer');
+//Run my sql connection in constant variable
+const mysqlConnection = require('../config/connection');
 let managersArr = [];
 let departmentsArr = [];
 let rolesArr = [];
 let employeesArr = [];
+
 
 /*------------------------Question Prompts based of selection------------------------ */
             /* ------------All constants are self explanitory-------------*/
@@ -141,3 +145,65 @@ const deleteEmployee = [
         choices: employeesArr
     }
 ];
+
+
+function quit () {
+    console.table(logo({
+        name: "Till next time!",
+    }).render());
+    process.exit();
+}
+
+/* ----------All Employee related functions-----------*/
+
+function deleteEmployee(name) {
+    const employeeName = name.split(" ");
+    mysqlConnection.query('DELETE FROM employee WHERE first_name = ? AND last_name = ?;', [employeeName[0], employeeName[1]], function (err, results) {
+        console.table(results);
+        start.start();
+    })
+}
+
+//will include later on to create full employee name category
+function getEmployeesToDelete (){
+    mysqlConnection.query('SELECT concat(first_name, " ", last_name) AS employee_name FROM employee;', function (err, results) {
+        employeesArr.length = 0;
+        for (const person in results) {
+            if (employeesArr.indexOf(results[person].employee_name) === -1) {
+                employeesArr.push(results[person].employee_name);
+            }
+        }
+        inquirer.prompt(deleteEmployeeQs)
+        .then((response) => {
+            deleteEmployee(response.name);
+        });        
+    })
+}
+
+/* ----------All Role related functions-----------*/
+function deleteRoles(role) {
+    mysqlConnection.query('DELETE FROM roles WHERE role_title = ?;', role, function (err, results) {
+        console.table(results);
+        start.start();
+    })
+}
+
+//if role is no longer needed, search through database for it's title and remove it
+function getRolesToDelete (){
+    mysqlConnection.query('SELECT role_title FROM roles', function (err, results) {
+        rolesArr.length = 0;
+        for (const role in results) {
+            if (rolesArr.indexOf(results[role].role_title === -1)) {
+                rolesArr.push(results[role].role_title);
+            }
+        }
+        inquirer.prompt(deleteRoleQs)
+        .then((response) => {
+            deleteRoles(response.role);
+        });        
+    })
+}
+
+/* ----------All Department related functions-----------*/
+
+
