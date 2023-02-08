02b230033614e1317d29ad5f5af5a9c9e6cb0274
John Leamy <jwleamy@gmail.com>
2023-02-07 16:26:30 -0800

    role values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,15 @@
 INSERT INTO department (name)
-VALUES ("Sales"),
-       ("Engineering"),
-       ("Finance"),
-       ("Legal");
+VALUES ("Sales"), /*1*/
+       ("Engineering"), /*2*/
+       ("Finance"), /*3*/
+       ("Legal"); /*4*/
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+       ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 4),
+       ("Legal Team Lead", 250000, 3),
+       ("Lawyer", 190000, 4);
